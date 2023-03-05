7eec19b17cea245dd5ab0615e072c30c624f9162
SantiagoCabreraEscoriza <santicorocottax@gmail.com>
2023-03-05 12:37:57 +0100

    FEAT checksession

--- src/api/user/user.controller.js
+++ src/api/user/user.controller.js
@@ -4,77 +4,96 @@ const { generateSign } = require("../../utils/jwt");
 
 const signUp = async (req, res) => {
 
-    try {
+  try {
 
-        const newUser = await new User(req.body);
+    const newUser = new User(req.body);
 
-        if (req.file) {
-          newUser.photo = req.file.path;
-        }
-
-        newUser.save();
-
-        return res.json(newUser);
-
-    } catch (error) {
-        return res.json({ mensaje: "error haciendo sign up", error: error })
+    if (req.file) {
+      newUser.photo = req.file.path;
     }
+
+    newUser.save();
+
+    return res.json(newUser);
+
+  } catch (error) {
+    return res.json({ mensaje: "error haciendo sign up", error: error })
+  }
 
 }
 
 const loginUser = async (req, res, next) => {
-    try {
-      const userDB = await User.findOne({ userName: req.body.userName }).populate({
-        path : 'hourDials',
-        populate : {
-          path : 'tasks'
-        }
-      });
-      if (!userDB) {
-        return res.json("Nombre de usuario incorrecto");
+  try {
+    const userDB = await User.findOne({ userName: req.body.userName }).populate({
+      path: 'hourDials',
+      populate: {
+        path: 'tasks'
       }
-      if (bcrypt.compareSync(req.body.password, userDB.password)) {
-        const token = generateSign(userDB._id, userDB.userName);
-        return res.status(200).json({token, userDB});
-      } else {
-        return res.json("contraseña incorrecta")
-      }
-    } catch (error) {
-      return res.json("error en el login")
+    });
+    if (!userDB) {
+      return res.json("Nombre de usuario incorrecto");
     }
-  };
-  
-  const updateUser = async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const putUser = new User(req.body);
-      putUser._id = id;
-      const UserDB = await User.findByIdAndUpdate(id, putUser, {new: true});
+    if (bcrypt.compareSync(req.body.password, userDB.password)) {
+      const token = generateSign(userDB._id, userDB.userName);
+      return res.status(200).json({ token, userDB });
+    } else {
+      return res.json("contraseña incorrecta")
+    }
+  } catch (error) {
+    return res.json("error en el login")
+  }
+};
+
+const updateUser = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const idString = JSON.stringify(req.user._id);
+    const idParsed = idParsed.slice(1, idString.length - 1);
+
+    if (idParsed === id) {
+      const UserDB = await User.findByIdAndUpdate(id, req.body, { new: true });
       if (!UserDB) {
         return res.json("error encontrando el usuario")
       }
       return res.status(200).json(UserDB);
-    } catch (error) {
-      return res.json("error actualizando el user");
+    } else {
+      return res.json("No puedes actualizar un usuario si no eres tu mismo")
     }
-  };
-  
-  const getUser = async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const userDB = await User.findById(id).populate({
-        path : 'hourDials',
-        populate : {
-          path : 'tasks'
-        }
-      });
-      if (!userDB) {
-        return res.json("error en el get del user")
-      }
-      return res.status(200).json(userDB);
-    } catch (error) {
-        return res.json("error en el get del user")
-    }
-  };
 
-module.exports = {signUp, getUser, updateUser, loginUser}
+  } catch (error) {
+    return res.json("error actualizando el user");
+  }
+};
+
+const getUser = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const userDB = await User.findById(id).populate({
+      path: 'hourDials',
+      populate: {
+        path: 'tasks'
+      }
+    });
+    if (!userDB) {
+      return res.json("error en el get del user")
+    }
+    return res.status(200).json(userDB);
+  } catch (error) {
+    return res.json("error en el get del user")
+  }
+};
+
+const checksession = async (req, res) => {
+
+  try {
+    
+    return res.json(req.user);
+
+  } catch (error) {
+    return res.json("fallo al checkear la sesión");
+  }
+
+}
+
+module.exports = { signUp, getUser, updateUser, loginUser, checksession }
--- src/api/user/user.routes.js
+++ src/api/user/user.routes.js
@@ -1,6 +1,6 @@
 const { isRegistered } = require("../../middlewares/auth");
 const upload = require("../../middlewares/file");
-const { signUp, loginUser, getUser, updateUser } = require("./user.controller");
+const { signUp, loginUser, getUser, updateUser, checksession } = require("./user.controller");
 
 const userRoutes = require("express").Router();
 
@@ -8,5 +8,6 @@ userRoutes.post("/", upload.single("photo"), signUp);
 userRoutes.post("/login", loginUser);
 userRoutes.get("/:id", [isRegistered], getUser);
 userRoutes.put("/:id", [isRegistered], upload.single("photo"), updateUser);
+userRoutes.put("/:id", [isRegistered], checksession);
 
 module.exports = userRoutes;
